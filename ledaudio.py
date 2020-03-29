952adbd5d65a9634664a9520b33ac0f7247a979e
Abe Binder <abebinder@gmail.com>
2020-03-29 02:10:10 -0700

    silence by 100 decibals

--- ledaudio.py
+++ ledaudio.py
@@ -2,21 +2,20 @@ import pydub;
 from pydub import AudioSegment
 
 
-def led(originalsong, interval_size_ms, percent_on, exportname):
+def led(originalsong, interval_size_ms, percent_on, exportname, stop_here=10000):
     print("importing")
     song = AudioSegment.from_mp3(originalsong)
     newSong = song[:1]
     print("starting slicing")
     quiet = True
-    for ms in range(interval_size_ms, len(song), interval_size_ms):
+    for ms in range(interval_size_ms, stop_here, interval_size_ms):
         print(ms)
         slice = song[ms-interval_size_ms: ms]
         if quiet:
-            slice-=6
+            slice-=100
         newSong = newSong +slice
         quiet=not quiet
     print("exporting")
     newSong.export(exportname, format="mp3")
 
-led("bonnie-tyler-i-need-a-hero-lyrics.mp3", 5000, 50, "secondmashup.mp3" )
 
