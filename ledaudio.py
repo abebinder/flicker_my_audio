35275c21ce989b5fcc54813b851f1df93215922d
Abe Binder <abebinder@gmail.com>
2020-03-29 02:24:21 -0700

    validate stop_here length

--- ledaudio.py
+++ ledaudio.py
@@ -4,9 +4,10 @@ from pydub import AudioSegment
 def led(originalsong, interval_size_ms, exportname, stop_here=10000):
     print("importing")
     song = AudioSegment.from_mp3(originalsong)
-    newSong = song[:1] # initialize new AudioSegment that is the first 1 ms of audiofile. (using audiosegment constructor didnt work)
+    newSong = song[:1] # initialize new AudioSegment that is the first 1 ms of audiofile. (using AudioSegment constructor didnt work)
     print("mixing audio")
     quiet = True
+    stop_here = min(stop_here, len(song))
     for ms in range(interval_size_ms, stop_here, interval_size_ms):
         print(ms)
         slice = song[ms-interval_size_ms: ms]
