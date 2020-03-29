e9aac205c85659c1985eb1fe100f349cf7d52721
Abe Binder <abebinder@gmail.com>
2020-03-29 02:30:25 -0700

    renamed some vars

--- ledaudio.py
+++ ledaudio.py
@@ -1,21 +1,21 @@
 from pydub import AudioSegment
 
 
-def led(originalsong, interval_size_ms, exportname, stop_here=10000):
+def led(original_audio_path, interval_size_ms, export_file_name, stop_here=10000):
     print("importing")
-    song = AudioSegment.from_mp3(originalsong)
-    newSong = song[:1] # initialize new AudioSegment that is the first 1 ms of audiofile. (using AudioSegment constructor didnt work)
+    original_audido = AudioSegment.from_mp3(original_audio_path)
+    new_audio = original_audido[:1] # initialize new AudioSegment that is the first 1 ms of audiofile. (using AudioSegment constructor didnt work)
     print("mixing audio")
     quiet = True
-    stop_here = min(stop_here, len(song))
+    stop_here = min(stop_here, len(original_audido))
     for ms in range(interval_size_ms, stop_here, interval_size_ms):
         print(ms)
-        slice = song[ms-interval_size_ms: ms]
+        slice = original_audido[ms-interval_size_ms: ms]
         if quiet:
             slice-=100 #mix 100 decibals lower than current volume
-        newSong = newSong +slice #append slice to new audido file
+        new_audio = new_audio +slice #append slice to new audido file
         quiet = not quiet # cycle between quiet and nonquiet sections
     print("exporting")
-    newSong.export(exportname, format="mp3")
+    new_audio.export(export_file_name, format="mp3")
 
 
