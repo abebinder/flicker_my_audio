42a92701eba627eae07c475321a3b882757ad610
Abe Binder <abebinder@gmail.com>
2020-03-29 02:18:38 -0700

    removed useless paraameter, added comments

--- ledaudio.py
+++ ledaudio.py
@@ -1,20 +1,19 @@
-import pydub;
 from pydub import AudioSegment
 
 
-def led(originalsong, interval_size_ms, percent_on, exportname, stop_here=10000):
+def led(originalsong, interval_size_ms, exportname, stop_here=10000):
     print("importing")
     song = AudioSegment.from_mp3(originalsong)
-    newSong = song[:1]
-    print("starting slicing")
+    newSong = song[:1] # initialize new AudioSegment that is the first 1 ms of audiofile. (using audiosegment constructor didnt work)
+    print("mixing audio")
     quiet = True
     for ms in range(interval_size_ms, stop_here, interval_size_ms):
         print(ms)
         slice = song[ms-interval_size_ms: ms]
         if quiet:
-            slice-=100
-        newSong = newSong +slice
-        quiet=not quiet
+            slice-=100 #mix 100 decibals lower than current volume
+        newSong = newSong +slice #append slice to new audido file
+        quiet = not quiet # cycle between quiet and nonquiet sections
     print("exporting")
     newSong.export(exportname, format="mp3")
 
